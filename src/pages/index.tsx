e8933945fa5e1f49744f4dc3d92d40a4aee25851
reginpv <reginpv@gmail.com>
2025-06-06 21:45:51 +0800

    add assets, setup globals

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function Home() {
   return (
-    <main>
+    <main className={`${inter.className}`}>
       Main
     </main>
   )
